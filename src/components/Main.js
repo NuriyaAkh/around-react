9c815aee9bb31f7aa0aa75127890d218985f5226
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-05-24 23:06:54 -0400

    fix: Set up the location cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -57,7 +57,28 @@ return (
         </div>
       </section>
       <section className="cards">
-        <ul className="cards__container"></ul>
+        <ul className="cards__container">
+          {cards.map((card)=>{
+            return(
+              
+            <li className="card">
+              <img class="card__img" src={card.link} alt={card.name}/>
+              <button aria-label="delete button" className="card__delete" type="button"></button>
+              <div className="card__info">
+                <h2 className="card__title">{card.name}</h2>
+                <div className="card__like">
+                <button aria-label="love it" type="button" className="card__button"></button>
+                <p class ="card__likes-counter">{card.likes.lenght}</p>
+                </div>
+              </div>
+            </li>
+         
+
+            )
+            
+
+          })}
+        </ul>
       </section>
     </main>
     
