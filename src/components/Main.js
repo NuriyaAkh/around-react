7f08e51ca05ba8f55c6a2465c6e821421888dd0d
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-05-26 18:27:40 -0400

    userData, setUserData

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,28 +5,27 @@ import { api } from "../utils/api";
 import Card from "./Card";
 
 export default function Main({ onEditProfileClick, onAddPlaceClick, onEditAvatarClick, onCardClick,onDeleteCardClick }) {
-  //Better to make one state variable user instead of declaring all the user properties. Imagine if there were a lot of properties. You would need to list them all in that case. Besides, in the future, you will need user id.
-  // const[userData, setUserData] = useSate("");?
-  const [userName, setUserName] = useState("");
-  const [userDescription, setUserDescription] = useState("");
-  const [userAvatar, setUserAvatar] = useState("");
+  
+  const [userData, setUserData] = useState({});
+  //const [userName, setUserName] = useState("");
+  // const [userDescription, setUserDescription] = useState("");
+  // const [userAvatar, setUserAvatar] = useState("");
   const [cards, setCards] = useState([]);
-
+  
  
   useEffect(() => {
     api.getUserData()
     .then((data) => {
       //console.log(data);
-      //setUserData(data?)
-      setUserName(data.name);
-      setUserDescription(data.about);
-      setUserAvatar(data.avatar);
+      setUserData(data)
+      //setUserName(data.name);
+      // setUserDescription(data.about);
+      // setUserAvatar(data.avatar);
     })
     .catch((err) => console.error(`Error while executing: ${err}`));
   }, []);
   useEffect(() => {
-    api
-  .getInitialCards()
+    api.getInitialCards()
   .then((data) => {
     setCards(data);
   })
@@ -38,16 +37,16 @@ export default function Main({ onEditProfileClick, onAddPlaceClick, onEditAvatar
       <main className="content">
         <section className="profile">
           <div className="profile__img-container">
-            <img className="profile__img" src={userAvatar} alt="profile image" />
+            <img className="profile__img" src={userData.avatar} alt="profile image" />
             <div className="profile__img-edit-logo">
               <img className="profile__img-icon" src={EditIcon} alt="edit avatar icon" onClick={onEditAvatarClick} />
             </div>
           </div>
           <div className="profile__info">
             <div className="profile__text">
-              <h1 className="profile__name">{userName}</h1>
+              <h1 className="profile__name">{userData.name }</h1>
               <button aria-label="edit" type="button" className="profile__button-name-edit" onClick={onEditProfileClick}/>
-              <p className="profile__about">{userDescription}</p>
+              <p className="profile__about">{userData.about}</p>
             </div>
 
             <button aria-label="add" type="button" className="profile__button-add" onClick={onAddPlaceClick}>
