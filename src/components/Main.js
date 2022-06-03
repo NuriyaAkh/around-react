d37a2dc09514bb889a702fa3f1d7517038bdc682
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-06-03 18:43:47 -0400

    Current user context, Use context in Main, Use context in Card

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,55 +1,80 @@
-import EditIcon from "../images/edit-avatar-logo.svg";
-import AddSign from "../images/+.svg";
-import React, {useEffect, useState} from "react";
-import { api } from "../utils/api";
-import Card from "./Card";
+import EditIcon from '../images/edit-avatar-logo.svg';
+import AddSign from '../images/+.svg';
+import React, {useEffect, useState} from 'react';
+import {api} from '../utils/api';
+import Card from './Card';
 
-export default function Main({ onEditProfileClick, onAddPlaceClick, onEditAvatarClick, onCardClick,onDeleteCardClick }) {
-  
+export default function Main({
+  onEditProfileClick,
+  onAddPlaceClick,
+  onEditAvatarClick,
+  onCardClick,
+  onDeleteCardClick,
+}) {
   const [userData, setUserData] = useState({});
   //const [userName, setUserName] = useState("");
   // const [userDescription, setUserDescription] = useState("");
   // const [userAvatar, setUserAvatar] = useState("");
   const [cards, setCards] = useState([]);
-  
- 
+
   useEffect(() => {
-    api.getUserData()
-    .then((data) => {
-      //console.log(data);
-      setUserData(data)
-      //setUserName(data.name);
-      // setUserDescription(data.about);
-      // setUserAvatar(data.avatar);
-    })
-    .catch((err) => console.error(`Error while executing: ${err}`));
+    api
+      .getUserData()
+      .then((data) => {
+        //console.log(data);
+        setUserData(data);
+        //setUserName(data.name);
+        // setUserDescription(data.about);
+        // setUserAvatar(data.avatar);
+      })
+      .catch((err) => console.error(`Error while executing: ${err}`));
   }, []);
   useEffect(() => {
-    api.getInitialCards()
-  .then((data) => {
-    setCards(data);
-  })
-  .catch((err) => console.error(`Error while executing: ${err}`));
+    api
+      .getInitialCards()
+      .then((data) => {
+        setCards(data);
+      })
+      .catch((err) => console.error(`Error while executing: ${err}`));
   }, []);
-  
+
   return (
     <>
       <main className="content">
         <section className="profile">
           <div className="profile__img-container">
-            <img className="profile__img" src={userData.avatar} alt="profile image" />
+            <img
+              className="profile__img"
+              src={userData.avatar}
+              alt="profile image"
+            />
             <div className="profile__img-edit-logo">
-              <img className="profile__img-icon" src={EditIcon} alt="edit avatar icon" onClick={onEditAvatarClick} />
+              <img
+                className="profile__img-icon"
+                src={EditIcon}
+                alt="edit avatar icon"
+                onClick={onEditAvatarClick}
+              />
             </div>
           </div>
           <div className="profile__info">
             <div className="profile__text">
-              <h1 className="profile__name">{userData.name }</h1>
-              <button aria-label="edit" type="button" className="profile__button-name-edit" onClick={onEditProfileClick}/>
+              <h1 className="profile__name">{userData.name}</h1>
+              <button
+                aria-label="edit"
+                type="button"
+                className="profile__button-name-edit"
+                onClick={onEditProfileClick}
+              />
               <p className="profile__about">{userData.about}</p>
             </div>
 
-            <button aria-label="add" type="button" className="profile__button-add" onClick={onAddPlaceClick}>
+            <button
+              aria-label="add"
+              type="button"
+              className="profile__button-add"
+              onClick={onAddPlaceClick}
+            >
               <img className="profile__add-img" src={AddSign} alt="add sidn" />
             </button>
           </div>
@@ -58,7 +83,14 @@ export default function Main({ onEditProfileClick, onAddPlaceClick, onEditAvatar
           <ul className="cards__container">
             {cards.map((card) => {
               //console.log(card);
-              return <Card key={card._id} card={card} onCardClick={onCardClick} onDeleteCardClick ={onDeleteCardClick}/>;
+              return (
+                <Card
+                  key={card._id}
+                  card={card}
+                  onCardClick={onCardClick}
+                  onDeleteCardClick={onDeleteCardClick}
+                />
+              );
             })}
           </ul>
         </section>
