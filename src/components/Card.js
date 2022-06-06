a3208a1423ce69e59a9d2d784494c2b830170429
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-06-06 10:02:35 -0400

    feature trash icon is visible to the owner of the card, todo delete feature

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,36 @@
 import React from 'react';
-import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import {CurrentUserContext} from '../contexts/CurrentUserContext';
 
+export default function Card({
+  card,
+  onCardClick,
+  onCardLike,
+  onDeleteCardClick,
+}) {
+  const currentUser = React.useContext(CurrentUserContext);
+  // Checking if the current user is the owner of the current card
+  const isOwn = card.owner._id === currentUser._id;
+  // Check if the card was liked by the current user
+  const isLiked = card.likes.some((user) => user._id === currentUser._id);
 
-export default function Card({card, onCardClick,onCardLike, onDeleteCardClick}) {
-    
- const currentUser = React.useContext(CurrentUserContext);
-// Checking if the current user is the owner of the current card
-const isOwn = card.owner._id === currentUser._id;
-// Check if the card was liked by the current user
-const isLiked = card.likes.some(user => user._id === currentUser._id);
+  // Creating a variable which you'll then set in `className` for the delete button
+  const cardDeleteButtonClassName = `card__delete ${
+    isOwn ? 'card__delete' : 'card__delete_hidden'
+  }`;
+  // Create a variable which you then set in `className` for the like button
+  const cardLikeButtonClassName = `card__button ${
+    isLiked && `card__button_active`
+  }`;
 
-// Creating a variable which you'll then set in `className` for the delete button
-const cardDeleteButtonClassName = (
-  `card__delete ${isOwn ? 'card__delete_visible' : 'card__delete_hidden'}`
-);
-// Create a variable which you then set in `className` for the like button
-const cardLikeButtonClassName = (`card__button ${isLiked &&`card__button_active`}`);
-
-function handleImageClick() {
-  onCardClick(card);
-}
-function handleDeleteClick() {
-  onDeleteCardClick(card);
-}
-function handleLikeClick(){
-  onCardLike(card);
-}
+  function handleImageClick() {
+    onCardClick(card);
+  }
+  function handleDeleteClick() {
+    onDeleteCardClick(card);
+  }
+  function handleLikeClick() {
+    onCardLike(card);
+  }
 
   return (
     <li className="card">
@@ -39,7 +44,7 @@ function handleLikeClick(){
         aria-label="delete image button"
         className={cardDeleteButtonClassName}
         type="button"
-        onClick ={handleDeleteClick}
+        onClick={handleDeleteClick}
       />
       <div className="card__info">
         <h2 className="card__title">{card.name}</h2>
