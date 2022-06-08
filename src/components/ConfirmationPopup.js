f0d962a1d279af9115198a2d4704600f5e863fee
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-06-08 10:16:26 -0400

    delete card not working fix

--- src/components/ConfirmationPopup.js
+++ src/components/ConfirmationPopup.js
@@ -2,11 +2,12 @@ import PopupWithForm from "./PopupWithForm";
 import {api} from '../utils/api';
 import { useState } from "react";
 export default function ConfirmationPopup({isOpen, onUpdate,onClose}){
-const [deleteCard, setDeleteCards]=useState(null);
+const [deleteCard, setDeleteCard]=useState(null);
   function handleCardDeleteSubmit(evt){
     evt.preventDefault(); 
     console.log("delete1");
     onUpdate({deleteCard})
+    setDeleteCard(null);
   }
 
 return (
